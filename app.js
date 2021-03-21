4095f0acfaca42d811c48ca77a635f57cf91b7f9
Adam Hott <adamhott@hotmail.com>
2021-03-21 07:08:29 -0500

    updated list

--- app.js
+++ app.js
@@ -3,17 +3,53 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
 
     var today = new Date();
-    var currentDay = today.getDay();
 
-    if(currentDay === 6 || currentDay === 0) {
-        res.sendFile(__dirname + "/weekend.html");
-    } else {
-        res.sendFile(__dirname + "/weekday.html");
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
 
+    var day = today.toLocaleDateString("en-US", options);
+
+  /*  var currentDay = today.getDay();
+    var day = "";
+
+    switch(currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        case 0:
+            day = "Sunday";
+            break;
+        default:
+            console.log("Error: current day is equal to: " + currentDay);
+    }
+*/
+    res.render("list", {kindOfDay: day})
 })
 
 app.listen(3000,function() {
